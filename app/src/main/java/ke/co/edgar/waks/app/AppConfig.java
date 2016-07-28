308454a6ba9bf01c660f10220a9c583017abc30e
edgar chris <edgarchris4@gmail.com>
2016-07-28 16:36:25 +0300

    initial commit

---
+++ app/src/main/java/ke/co/edgar/waks/app/AppConfig.java
@@ -0,0 +1,9 @@
+package ke.co.edgar.waks.app;
+
+public class AppConfig {
+	// Server user login url
+	public static String URL_LOGIN = "http://10.0.3.2/android_login_api/login.php";
+
+	// Server user register url
+	public static String URL_REGISTER = "http://10.0.3.2/android_login_api/register.php";
+}
